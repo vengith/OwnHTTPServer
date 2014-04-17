27f5463f3fd4e7cac9d63b5f939082c509ef212a
Chaosteilchen <Michael.Weiss.1991@gmail.com>
2014-04-17 15:55:42 +0200

    Vorlesung 2

--- src/simplewebserver/MySimpleRequest.java
+++ src/simplewebserver/MySimpleRequest.java
@@ -1,5 +1,21 @@
 package simplewebserver;
 
-public class MySimpleRequest {
+import givenpackage.InvalidRequestException;
+
+import java.io.IOException;
+
+import create.AbstractSimpleRequest;
+
+public class MySimpleRequest extends AbstractSimpleRequest{
+
+	@Override
+	public void parseRequest() throws IOException, InvalidRequestException {
+		String line = null;
+		boolean firstLine = true;
+		
+		//Lies es so lange von Socket, bis nichts mehr da ist...
+		
+		while( line = in.readLine() ) 
+	}
 
 }
--- src/simplewebserver/MySimpleResponse.java
+++ src/simplewebserver/MySimpleResponse.java
@@ -1,5 +1,21 @@
 package simplewebserver;
 
-public class MySimpleResponse {
+import java.io.IOException;
+
+import create.AbstractSimpleResponse;
+
+public class MySimpleResponse extends AbstractSimpleResponse {
+
+	@Override
+	public void addToBody(String string) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendHeader() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
