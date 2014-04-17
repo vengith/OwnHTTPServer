f421303f3ac8e72a5430265f48c993d48239d439
Chaosteilchen <Michael.Weiss.1991@gmail.com>
2014-04-17 15:51:53 +0200

    Schrott in Vorlesung

--- src/simplewebserver/MyRessourceManager.java
+++ src/simplewebserver/MyRessourceManager.java
@@ -1,5 +1,27 @@
 package simplewebserver;
 
-public class MyRessourceManager {
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import simplewebserver.MySimpleRequest;
+import simplewebserver.MySimpleResponse;
+
+public interface MyRessourceManager {
+
+	void doGet(AbstractSimpleRequest request, AbstractSimpleResponse response)
+			throws IOException;
+
+	void doService(AbstractSimpleRequest request, AbstractSimpleResponse response)
+			throws IOException;
+	
+	 void copyFile(File file, AbstractSimpleResponse response) throws IOException;
+
+	void copyStreams(InputStream in, OutputStream out) throws IOException;
+
+	void writeDirectoryAsHtml(File file, AbstractSimpleRequest request,
+			AbstractSimpleResponse response) throws IOException;
+
 
 }
